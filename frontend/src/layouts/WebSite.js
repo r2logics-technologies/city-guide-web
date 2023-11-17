fa825aa84149961e5a69a3ec17285f22364e133e
lunaram <lunagodara2016@gmail.com>
2023-11-17 11:58:45 +0530

    commit

--- frontend/src/layouts/WebSite.js
+++ frontend/src/layouts/WebSite.js
@@ -3,10 +3,10 @@ import "../assets/website/css/stylesheet.css";
 import "../assets/website/css/style.css";
 import "../assets/website/css/responsive.css";
 
-import Home from 'components/website/Home'
-import Footer from 'components/website/layouts/Footer'
-import Header from 'components/website/layouts/Header'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom';
+import Home from 'views/website/Home';
+import Header from 'views/website/layouts/Header';
+import Footer from 'views/website/layouts/Footer';
 
 const WebSite = () => {
   return (
