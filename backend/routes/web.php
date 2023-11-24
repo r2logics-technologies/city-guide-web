110488a42bc80e7c357c4ee422b211b8cd5a2bb5
Ram <ramchandrakumawat365@gmail.com>
2023-11-24 12:54:07 +0530

    ram commit

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,6 +18,42 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/migrate-refresh', function () {
+    Artisan::call('migrate:fresh --seed');
+});
+Route::get('/generate-key', function () {
+    Artisan::call('key:generate');
+});
+
+Route::get('/linkstorage', function () {
+    Artisan::call('storage:link');
+});
+
+Route::get('/dump-autoload', function () {
+    Artisan::call('clear-compiled');
+    exec('composer dump-autoload');
+    Artisan::call('optimize');
+    echo 'dump-autoload complete';
+});
+Route::get('/composer-update', function () {
+    echo 'please wait!! composer updating';
+    exec('composer update');
+    echo 'composer updated';
+});
+
+Route::get('/config-clear', function () {
+    Artisan::call('cache:clear');
+    Artisan::call('config:clear');
+    Artisan::call('route:clear');
+    return "Cache is cleared";
+});
+
+Route::get('/config/{command}', function ($command) {
+    Artisan::call("'" . $command . "'");
+    return "Cache is cleared";
 });
 
 Auth::routes();
--- frontend/src/views/admin/settings/Settings.js
+++ frontend/src/views/admin/settings/Settings.js
@@ -1,152 +1,123 @@
 import React, { useEffect, useState } from "react";
-import {
-  Modal,
-  ModalHeader,
-  ModalBody,
-  Card,
-  CardHeader,
-  CardBody,
-  CardTitle,
-  Row,
-  Col,
-} from "reactstrap";
-
-import Table from "components/table/Table";
-import * as AiIcons from "react-icons/ai";
-import api from "utility/api";
+import { Card, CardHeader, CardBody, CardTitle, Row, Col } from "reactstrap";
 import toast, { Toaster } from "react-hot-toast";
 import { Modal as antdModal } from "antd";
+import * as BsIcons from "react-icons/bs";
+import { useForm, Controller, useFieldArray } from "react-hook-form";
+import { Select } from "antd";
+import api from "utility/api";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import apiService from "utility/apiService";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import uploadImg from "assets/img/img-upload.png";
 
+const Option = Select.Option;
 const { confirm } = antdModal;
 
 function Settings() {
-  const [showDetails, setShowDetails] = useState();
-  const [booking, setBooking] = useState(null);
-  const handleShowForm = () => {
-    setShowDetails(!showDetails);
-    setBooking(null);
+  const navigate = useNavigate();
+  const params = useParams();
+  const postId = params.id;
+  const [imgUpload, setImgUpload] = useState(uploadImg);
+  const [thumbnailImgUrl, setthumbnailImgUrl] = useState("");
+
+  const selectThumbnailImg = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        setthumbnailImgUrl(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
   };
 
-  const [data, setData] = useState([]);
-  const [header, setHeader] = useState([
-    {
-      Header: "Name",
-      accessor: "name",
-      sortType: "alphanumeric",
-    },
-    {
-      Header: "Place",
-      accessor: "place",
-      sortType: "alphanumeric",
-    },
-    {
-      Header: "Booking at",
-      accessor: "booking_at",
-      sortType: "alphanumeric",
-    },
-    {
-      Header: "Status",
-      accessor: "status",
-      sortType: "alphanumeric",
-    },
-    {
-      Header: "Actions",
-      accessor: "actions",
-      disableSortBy: true,
-      Cell: ({ row }) => (
-        <div className="d-flex justify-content-start align-items-center gap-2">
-          <div className="d-flex justify-content-start gap-3">
-            <AiIcons.AiOutlineEye
-              className="text-info cr-pointer fs-4"
-              onClick={() => handleDetail(row)}
-            />
-          </div>
-          {row.original.status == "pending" ? (
-            <>
-              <div className="d-flex justify-content-start gap-3">
-                <AiIcons.AiOutlineCheck
-                  className="text-success cr-pointer fs-4"
-                  onClick={() =>
-                    handleChangeStatus(row.original.id, "activated")
-                  }
-                />
-              </div>
-              <div className="d-flex justify-content-start gap-3">
-                <AiIcons.AiOutlineClose
-                  className="text-danger cr-pointer fs-4"
-                  onClick={() =>
-                    handleChangeStatus(row.original.id, "deactivated")
-                  }
-                />
-              </div>
-            </>
-          ) : (
-            <div className="d-flex justify-content-start gap-3">
-              <AiIcons.AiOutlineDelete
-                className="text-danger cr-pointer fs-4"
-                onClick={() => handleChangeStatus(row.original.id, "deleted")}
-              />
-            </div>
-          )}
-        </div>
-      ),
-    },
-  ]);
+  const {
+    register,
+    control,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm();
 
-  const handleDetail = (row) => {
-    handleShowForm();
-    setBooking(row.original);
+  const removeThumImg = () => {
+    setthumbnailImgUrl("");
+    setValue("thumb", null);
   };
-  console.log(booking);
-  const statusChange = (id, status) => {
-    api
-      .post(`/api/admin/bookings/change/status/${id}`, { status: status })
-      .then((res) => {
-        const data = res.data;
-        if (data.status === "success") {
-          setTimeout(() => {
-            toast.success(
-              `Successfully ${
-                status == "activated" ? "accepted." : status == "deleted" ? "deleted." : "cancelled."
-              }.`
-            );
-            fetchData();
-          }, 1000);
-        } else {
-          setTimeout(() => {
-            toast.error(data.message);
-          }, 1000);
-        }
-      })
-      .catch((err) => {
-        console.error(err);
-        toast.error("Something went wrong!");
-      });
-  };
+  const [categoriesList, setCategoriesList] = useState([]);
 
-  const handleChangeStatus = (id, status) => {
-    confirm({
-      title: "Are you sure?",
-      content: `You want to ${
-        status == "activated" ? "accept this booking." : status == "deleted" ? "delete this booking." : "cancel this booking."
-      }`,
-      onOk() {
-        setTimeout(() => {
-          statusChange(id, status);
-        }, 2000);
-      },
-      onCancel() {},
-    });
-  };
+  useEffect(() => {
+    fetchCategoriesData();
+  }, []);
 
-  const fetchData = () => {
-    let url = "/api/admin/bookings";
+  useEffect(() => {
+    if (postId) fetchPlaceData(postId);
+  }, [postId]);
+
+  const fetchCategoriesData = () => {
+    let url = "/api/admin/posts/categories";
     api
       .get(url)
       .then((res) => {
         const data = res.data;
         if (data.status === "success") {
-          setData(data.bookings);
+          setCategoriesList(data.categories);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  const fetchPlaceData = (id) => {
+    let url = `/api/admin/post/${id}`;
+    api
+      .get(url)
+      .then((res) => {
+        const data = res.data;
+        console.log(data);
+        if (data.status === "success") {
+          const post = data.post;
+          setValue("title", post.title);
+          setValue("content", post.content);
+          setValue("post_category_id", post.post_category_id);
+          if (post.thumb != null) {
+            setImgUpload(apiService.ledgerUrl + post.thumb);
+          }
+        } else {
+          toast(data.message);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  const onSubmit = async (data) => {
+    console.log(data);
+    const formData = new FormData();
+    if (postId) {
+      formData.append("edited", postId);
+    }
+    formData.append("title", data.title != null && data.title);
+    formData.append("content", data.content != null && data.content);
+    formData.append(
+      "post_category_id",
+      data.post_category_id != null && data.post_category_id
+    );
+    formData.append("thumb", data.thumb[0] != null && data.thumb[0]);
+
+    const url = `/api/admin/posts/save/update`;
+    api
+      .post(url, formData)
+      .then((res) => {
+        const data = res.data;
+        if (data.status === "success") {
+          setTimeout(() => {
+            toast.success(data.message);
+            navigate("/admin/blog");
+          }, 1000);
         } else {
           toast.error(data.message);
         }
@@ -157,94 +128,161 @@ function Settings() {
       });
   };
 
-  useEffect(() => {
-    fetchData();
-  }, []);
-  const closeBtn = (
-    <AiIcons.AiOutlineCloseCircle
-      className="cr-pointer fs-4"
-      onClick={handleShowForm}
-    />
-  );
+  // Define your custom toolbar options
+  const toolbarOptions = [
+    [{ header: [1, 2, false] }],
+    ["bold", "italic", "underline", "strike"],
+    ["blockquote", "code-block"],
+    [{ list: "ordered" }, { list: "bullet" }],
+    [{ indent: "-1" }, { indent: "+1" }],
+    [{ align: [] }],
+    [{ script: "sub" }, { script: "super" }],
+  ];
+
   return (
     <>
+      <Toaster />
       <div className="content">
-        <Toaster />
         <Row>
           <Col md="12">
             <Card>
-              <CardHeader className="d-flex justify-content-between align-items-center">
-                <CardTitle tag="h4">Settings List </CardTitle>
+              <CardHeader className="border-bottom">
+                <CardTitle tag="h3">
+                  {postId ? "Edit" : "Create"} Post{" "}
+                </CardTitle>
               </CardHeader>
               <CardBody>
-                <Table data={data} header={header} />
+                <form onSubmit={handleSubmit(onSubmit)}>
+                  <div className="row">
+                    <div className="my-2 col-md-4">
+                      <div className="p-2 text-center position-relative">
+                        {thumbnailImgUrl ? (
+                          <>
+                            <BsIcons.BsX
+                              className="img-remove-btn"
+                              onClick={() => {
+                                removeThumImg();
+                              }}
+                            />
+                            <img
+                              src={thumbnailImgUrl}
+                              style={{ height: "120px", maxWidth: "100%" }}
+                            />
+                          </>
+                        ) : (
+                          <>
+                            <img
+                              src={imgUpload}
+                              style={{ height: "120px", maxWidth: "100%" }}
+                            />
+                          </>
+                        )}
+                        <input
+                          type="file"
+                          className="image-input"
+                          {...register("thumb")}
+                          accept="image/jpeg, image/jpg, image/png, application/pdf"
+                          onChange={selectThumbnailImg}
+                        />
+                        {!thumbnailImgUrl && (
+                          <div>
+                            <label className="mt-2">
+                              Select Thumbnail Image
+                            </label>
+                          </div>
+                        )}
+                      </div>
+                    </div>
+                    <div className="col-md-8">
+                      <div className="my-2">
+                        <div className="d-flex justify-content-between">
+                          <small className="text-muted required-field">
+                            Title
+                          </small>
+                          <small className="text-danger">
+                            {errors?.title && "Title is required"}
+                          </small>
+                        </div>
+                        <input
+                          className="form-control"
+                          type="text"
+                          {...register("title", { required: true })}
+                          placeholder="What the title of place"
+                        />
+                      </div>
+                      <div className="my-2">
+                        <div className="d-flex justify-content-between">
+                          <small className="text-muted required-field">
+                            Category
+                          </small>
+                          <small className="text-danger">
+                            {errors?.post_category_id && "Category is required"}
+                          </small>
+                        </div>
+                        <Controller
+                          name="post_category_id"
+                          control={control}
+                          rules={{ required: true }}
+                          render={({ field }) => (
+                            <Select
+                              className="w-100"
+                              placeholder="Select Category"
+                              {...field}
+                            >
+                              {categoriesList.map((category) => (
+                                <Option key={category.id} value={category.id}>
+                                  {category.name}
+                                </Option>
+                              ))}
+                            </Select>
+                          )}
+                        />
+                      </div>
+                      <div className="my-2">
+                        <div className="d-flex justify-content-between">
+                          <small className="text-muted">Content</small>
+                          <small className="text-danger">
+                            {errors?.content && "Content is required"}
+                          </small>
+                        </div>
+                        <Controller
+                          name="content"
+                          control={control}
+                          rules={{ required: true }}
+                          render={({ field }) => (
+                            <ReactQuill
+                              className="border rounded"
+                              value={field.value}
+                              onChange={(value) => field.onChange(value)}
+                              modules={{
+                                toolbar: toolbarOptions,
+                              }}
+                            />
+                          )}
+                        />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="d-flex border-top mt-4 justify-content-end gap-3">
+                    <button
+                      className="btn btn-outline-primary rounded-pill"
+                      type="submit"
+                    >
+                      Submit
+                    </button>
+                    <Link
+                      to="/admin/blog"
+                      className="btn btn-outline-danger rounded-pill"
+                    >
+                      Cancel
+                    </Link>
+                  </div>
+                </form>
               </CardBody>
             </Card>
           </Col>
         </Row>
       </div>
-      {/* Form Modal */}
-      <Modal
-        isOpen={showDetails}
-        toggle={handleShowForm}
-        size="md"
-        fade={true}
-        centered={true}
-      >
-        <ModalHeader close={closeBtn}>Booking Details</ModalHeader>
-        <ModalBody>
-          <div className="table-responsive">
-            <table className="table table-bordered">
-              <tr>
-                <th>Name:</th>
-                <td>{booking?.name}</td>
-              </tr>
-              <tr>
-                <th>Email:</th>
-                <td>{booking?.email}</td>
-              </tr>
-              <tr>
-                <th>Mobile:</th>
-                <td>{booking?.mobile}</td>
-              </tr>
-              <tr>
-                <th>Booking Place:</th>
-                <td>{booking?.place}</td>
-              </tr>
-              <tr>
-                <th>Number of Adults:</th>
-                <td>{booking?.adults}</td>
-              </tr>
-              <tr>
-                <th>Number of Children:</th>
-                <td>{booking?.childs}</td>
-              </tr>
-
-              
-              <tr>
-                <th>Booking Date:</th>
-                <td>{booking?.booking_date}</td>
-              </tr>
-              <tr>
-                <th>Booking Time:</th>
-                <td>{booking?.booking_time}</td>
-              </tr>
-              <tr>
-                <th>Booking At:</th>
-                <td>{booking?.booking_at}</td>
-              </tr>
-              <tr>
-                <th>Status:</th>
-                <td>{booking?.status}</td>
-              </tr>
-              <tr>
-                <th>Message:</th>
-                <td>{booking?.message}</td>
-              </tr>
-            </table>
-          </div>
-        </ModalBody>
-      </Modal>
     </>
   );
 }
